336844d86049a004af735e043bfafebd7e5b7a27
Jan Hagen Clausen <prances_lens_0t@icloud.com>
2026-01-06 21:18:43 +0100

    feat(project): Define centralized Constants enum

---
+++ LANImageUploader/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  LANImageUploader
+//
+//  Created by AI on 06/01/2026.
+//
+
+import Foundation
+
+enum Constants {
+    enum Keychain {
+        static let serverPassword = "serverPassword"
+    }
+    
+    enum UserDefaults {
+        static let serverSettings = "serverSettings"
+        static let onboardingCompleted = "onboardingCompleted"
+        static let archiveCustomNames = "archiveCustomNames"
+    }
+    
+    enum Notifications {
+        static let networkStatusChanged = "NetworkMonitorStateChanged"
+        static let archivedImageDeleted = "ArchivedImageDeleted"
+    }
+    
+    enum BackgroundTasks {
+        static let dailyImageSave = "com.janhagenclausen.LANImageUploader.dailyImageSave"
+    }
+}
